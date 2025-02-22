60113de943efebae3613b8213c0c771fed20410f
RajaPriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-02-22 11:40:41 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,257 @@
+export const Bio = {
+  name: "Rajapriyanka R",
+  roles: [
+    "Full Stack Developer",
+       "Programmer",
+  ],
+  description: "I am a highly motivated and adaptable individual with a strong drive for learning and growth. Passionate about problem-solving and innovation, I strive to deliver high-quality results in every task I undertake. With a proactive mindset and a positive attitude, I am eager to contribute effectively and make a meaningful impact.",
+  github: "https://github.com/rajapriyanka",
+  resume:
+ " https://drive.google.com/file/d/1GFgycIhFza338DYYRsjHR1yW3MRLErC9/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/rajapriyankar/",
+  email: "rajapriyanka1101@gmail.com",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Thymeleaf",
+        image: "https://www.thymeleaf.org/images/thymeleaf.png"
+      },
+        
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+    
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+     
+     
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Spring Boot",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original-wordmark.svg",
+      },
+      
+      
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    
+    ],
+  },
+ 
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image: "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "Spring Tool Suite",
+        image: "https://avatars.githubusercontent.com/u/317776?s=200&v=4"
+      },
+      
+      {
+        name: "VS Code",
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg",
+      },
+      {
+        name: "Postman",
+        image: "https://www.svgrepo.com/show/354202/postman-icon.svg",
+      }
+    ]
+  },
+  
+  
+];
+
+export const experiences = [
+ 
+  {
+    id: 1,
+    role: "Java Full Stack Developer Intern",
+    company: "Infosys Springboard",
+    date: "November 2024 - January 2025",
+    desc: "Completed training in Full Stack Development with Java, Spring Boot, and ReactJS through Infosys Springboard. Developed the 'All is Well Pharma Automation System' to streamline pharmaceutical processes.",
+    img: "/Info.png",
+    skills: [
+      "Java",
+      "Spring Boot",
+      "ReactJS",
+      "MySQL",
+      "HTML",
+      "CSS",
+      "JavaScript"
+    ]
+},
+  {
+    id: 2,
+    role: "Web Development Intern",
+    company: "Unified Mentor",
+    date: "May 2024 - June 2024",
+    desc: "Completed a one-month internship at Unified Mentor, where I learned front-end web development, including modern UI/UX principles and responsive design.",
+    img: "/Unified.jpeg",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "ReactJS"
+    ]
+},
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "/pits.jpeg",
+    school: "Parisutham Institute of Technology and Science, Thanjavur",
+    date: "Nov 2021 - May 2025",
+    grade: "8.33 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Parisutham Institute of Technology and Science, Thanjavur. I have completed multiple semesters with a CGPA of 8.33. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I have also actively participated in technical communities and associations, enhancing my knowledge and practical skills in Full Stack Development.",
+    degree: "Bachelor of Engineering - BE, Computer Science and Engineering"
+},
+{
+  id: 1,
+  img:"/kalaimagal.jpeg",
+  school: "Kalaimagal Higher Secondary School, Thanjavur",
+  date: "Jun 2020 - May 2021",
+  grade: "88%",
+  desc: "I completed my Class 12 education at Kalaimagal Higher Secondary School, Thanjavur, where I studied Bio Maths.",
+  degree: "HSLC (XII)"
+},
+{
+  id: 2,
+  img: "/don bosko.png",
+  school: "Don Bosco School, Trichy",
+  date: "Jun 2018 - May 2019",
+  grade: "91%",
+  desc: "I completed my Class 10 education at Don Bosco School, Trichy, where I studied Science with Computer Applications.",
+  degree: "SSLC "
+},
+
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "E-Commerce Application",
+    date: "Jun 2023 - Jul 2023",
+    description: "Developed a responsive E-Commerce web application with user authentication, shopping cart functionality, and payment integration. Implemented front-end features using HTML, CSS, and JavaScript.",
+    image: "/ecommerce.jpeg",
+    tags: ["HTML", "CSS", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/rajapriyanka/E-Commerce-Application-On-IBM-Cloud-Foundry"
+  },
+  {
+    id: 1,
+    title: "Banking Website",
+    date: "Aug 2023 - Sep 2023",
+    description: "Developed a full-stack banking web application with user authentication, account management, and transaction features. Implemented role-based access for admins and users.",
+    image: "/Banking.png",
+    tags: ["Spring Boot", "Thymeleaf", "MySQL", "Postman"],
+    category: "web app",
+    github: "https://github.com/rajapriyanka/Riyadhya-Banking-Website.git"
+  },
+  {
+    id: 2,
+    title: "All is Well Pharmacy Management System",
+    date: "Oct 2023 - Dec 2023",
+    description: "Built a pharmacy management system that supports CRUD operations for medicines, billing, and automatic reorder notifications. Integrated MySQL database for efficient data storage.",
+   image: "/Pharma.png",
+    tags: ["Spring Boot", "Thymeleaf", "MySQL", "Postman"],
+    category: "web app",
+    github: "https://github.com/rajapriyanka/Pharma-Automation-System.git"
+},
+{
+  id: 1,
+  title: "Weather Web",
+  date: "Jul 2023",
+  description: "A simple web-based application that allows users to check the current weather of any city using the OpenWeatherMap API. Features include real-time weather data, temperature display in Celsius, and a clean UI.",
+  image: "https://github.com/user-attachments/assets/75240219-63f0-4971-86a4-c917155b0ba3",
+  tags: ["HTML", "CSS", "JavaScript", "OpenWeatherMap API"],
+  category: "web app",
+  github: "https://github.com/rajapriyanka/Weather-Web.git",
+  webapp: "https://rajapriyanka.github.io/Weather-Web/"
+},
+{
+  id: 2,
+  title: "English Dictionary Web App",
+  date: "Aug 2023",
+  description: "An interactive dictionary web app that allows users to search for word meanings and listen to pronunciations using the Dictionary API. Features include real-time word search, meaning display, and audio pronunciation.",
+  image: "https://github.com/user-attachments/assets/1a4c1cd7-6524-48af-a0b1-4d86a5666905",
+  tags: ["HTML", "CSS", "JavaScript", "Dictionary API"],
+  category: "web app",
+  github: "https://github.com/rajapriyanka/English-Dictionary.git",
+  webapp: "https://english-dictionary-cyan.vercel.app/"
+},
+{
+  id: 3,
+  title: "Expense Tracker Web App",
+  date: "Sep 2023",
+  description: "A simple and interactive web application that allows users to record, track, and manage their expenses efficiently. Features include real-time total calculation, local storage support, and an intuitive UI.",
+  image: "https://github.com/user-attachments/assets/836587c8-d0c4-43ef-bc32-8777d5fe8073",
+  tags: ["HTML", "CSS", "JavaScript", "LocalStorage API"],
+  category: "web app",
+  github: "https://github.com/rajapriyanka/Expense-Tracker.git",
+  webapp: "https://rajapriyanka.github.io/Expense-Tracker/"
+},
+
+
+];
+
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
