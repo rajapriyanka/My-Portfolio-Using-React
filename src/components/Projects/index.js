10f9566cc42de58c95102db7d897bb49899286a5
RajaPriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-04-03 07:23:31 +0530

    Update index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -12,7 +12,7 @@ const Projects = ({openModal,setOpenModal}) => {
       <Wrapper>
         <Title>Projects</Title>
         <Desc>
-          I have worked on a wide range of projects. Here are some of my projects.
+      I have undertaken a diverse array of projects, demonstrating expertise across various domains. Below are some of my notable works.
         </Desc>
         <ToggleButtonGroup >
          
