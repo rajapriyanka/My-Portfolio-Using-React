b331180dc333d3360c8ddb76986a4a6226477b27
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-06-16 09:27:02 +0530

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -24,7 +24,7 @@ const Navbar = () => {
         </MobileIcon>
         <NavItems>
           <NavLink href="#about">About </NavLink>
-          <NavLink href='#skills'>Skill Set</NavLink>
+          <NavLink href='#skills'>Skill </NavLink>
           <NavLink href='#experience'>Experience</NavLink>
           <NavLink href='#projects'>Projects</NavLink>
           <NavLink href='#education'>Education</NavLink>
