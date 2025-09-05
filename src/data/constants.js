c480158703123c3a17c120a2b8fcb47616f69d05
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-05 21:12:10 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -280,8 +280,6 @@ export const projects = [
  
 },
 
-
-
 ];
 
 
@@ -292,6 +290,7 @@ export const TimeLineData = [
   { year: 2020, text: "Shared my projects with the world" },
   { year: 2021, text: "Started my own platform" },
 ];
+
 
 
 
