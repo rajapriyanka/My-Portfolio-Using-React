2dfddc9bb989b4ca74bf1e1e3b66660a1d69d0ed
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-10 20:07:13 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -79,7 +79,7 @@ skills: [
   },
 {
   name: "MQTT",
-  image: "https://cdn.worldvectorlogo.com/logos/mqtt.svg",
+  image: "https://mqtt.org/assets/downloads/mqtt-logo.png",
 },
   {
     name: "Node.js",
@@ -303,6 +303,7 @@ export const TimeLineData = [
   { year: 2020, text: "Shared my projects with the world" },
   { year: 2021, text: "Started my own platform" },
 ];
+
 
 
 
